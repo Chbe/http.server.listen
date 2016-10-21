fa9fe63e1992474686478a05de4c18116dbc1ac9
Christopher <bengtsson.christopher@hotmail.com>
2016-10-21 20:06:43 +0000

    initial file commit

--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -1,12 +1,16 @@
 myApp.controller('homeController', ['$scope', function($scope){
     $scope.myInterval = 3000;
     $scope.slides = [{
-        image: "img/Androidpic.png",
-        link: "https://developer.android.com"
+        image: "img/socketio.png",
+        link: "http://socket.io/"
     },
     {
         image: "img/nodejs.png",
         link: "https://nodejs.org"
+    },
+    {
+        image: "img/Androidpic.png",
+        link: "https://developer.android.com"
     }]
     
 }]);
