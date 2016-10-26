291b82b044031378915745f1e3fcc9a32fc35d96
Christopher <bengtsson.christopher@hotmail.com>
2016-10-26 21:44:05 +0000

    linked to flocker

--- client/js/app.js
+++ client/js/app.js
@@ -10,6 +10,7 @@ var myApp = angular.module('myApp', [
         $routeProvider.when('/projects', {templateUrl: '/partials/projects.html', controller: 'projectsController'});
         $routeProvider.when('/projects/customerapi', {templateUrl: '/partials/projects/customerapi.html', controller: 'customerApiController'});
         $routeProvider.when('/projects/chat', {templateUrl: '/partials/projects/chat.html', controller: 'chatController'});
+        $routeProvider.when('/Public/index', {templateUrl: '/Android/Public/index.html', controller: 'main'});
         
         //if no valid routes are found, redirect to /home
         $routeProvider.otherwise({redirectTo: '/home'});
